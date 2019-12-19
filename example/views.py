d07b3712c24ca4e40809a6642ec778322d2fba8d
Artur Beltsov <artur1998g@gmail.com>
2019-12-19 14:44:54 +0500

    changes

--- example/views.py
+++ example/views.py
@@ -1,7 +1,9 @@
 import gc
 import datetime
+from collections import Counter
 
 from django.http import JsonResponse, HttpRequest
+from django.conf import settings
 
 
 def example_view(request: HttpRequest):
@@ -10,3 +12,13 @@ def example_view(request: HttpRequest):
         'path': request.path,
     }
     return JsonResponse(data)
+
+
+def middlewares_view(request: HttpRequest):
+    return JsonResponse(list(settings.MIDDLEWARE_CLASSES), safe=False)
+
+
+def garbage_output_view(request: HttpRequest):
+    c = Counter(type(o) for o in gc.get_objects() if 'instana' in str(type(o)).lower())
+    data = {str(i): j for i, j in c.most_common(None)}
+    return JsonResponse(data, safe=False)
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from example.views import example_view
+from example.views import example_view, garbage_output_view, middlewares_view
 
 urlpatterns = [
-    url(r'^.*$', example_view)
+    url(r'^middleware/*?$', middlewares_view),
+    url(r'^gc/*?$', garbage_output_view),
+    url(r'^.*$', example_view),
 ]
