31522c5d33ad72d782ab07aaaf8c9eb2eb3901c9
Artur Beltsov <artur1998g@gmail.com>
2019-12-20 11:16:31 +0500

    .

--- example/views.py
+++ example/views.py
@@ -1,5 +1,6 @@
 import gc
 import datetime
+import tracemalloc
 from collections import Counter
 
 from django.http import JsonResponse, HttpRequest
@@ -31,3 +32,46 @@ def garbage_output_view(request: HttpRequest):
         'objects': instana_objects,
     }
     return JsonResponse(data, safe=False)
+
+
+def trace_start(request: HttpRequest):
+    tracemalloc.start()
+    return JsonResponse({}, safe=False)
+
+
+check = None
+def trace(request: HttpRequest):
+    # gc.collect()
+    global check
+    snapshot = tracemalloc.take_snapshot()
+    tracemalloc.get_tracemalloc_memory()
+    statistics = list(filter(lambda s: 'tracemalloc' not in str(s.traceback),
+                      sorted(snapshot.statistics('lineno'), key=lambda s: s.count, reverse=True)))
+    statistic = {str(s.traceback): s.count for s in statistics}
+    if check is None:
+        check = statistic
+        data = {}
+    else:
+        temp = statistic.copy()
+        for key, val in statistic.items():
+            count = check.pop(key, 0)
+            if val - count:
+                check[key] = val - count
+        data = check
+        check = temp
+    checks = [
+        'trace',
+    ]
+    objects = []
+    for s in statistics:
+        s = str(s)
+        for ch in checks:
+            if ch in s:
+                objects.append(s)
+    mem1, mem2 = tracemalloc.get_traced_memory()
+    top = list(map(str, statistics[:10]))
+    return JsonResponse(dict(mem1=mem1, mem2=mem2, top=top, objects=objects, data=data), safe=False)
+
+
+tracemalloc.start()
+gc.disable()
--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from example.views import example_view, garbage_output_view, middlewares_view
+from example.views import example_view, garbage_output_view, middlewares_view, trace_start, trace
 
 urlpatterns = [
     url(r'^middleware/*?$', middlewares_view),
     url(r'^gc/*?$', garbage_output_view),
+    url(r'^start.*', trace_start),
+    url(r'^trace.*', trace),
     url(r'^.*$', example_view),
 ]
